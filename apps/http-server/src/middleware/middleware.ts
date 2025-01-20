885676a392b49af3e8ed7e258cb818503aff1863
ghagevaibhav <ghagevaibhav@gmail.com>
2025-01-21 01:58:08 +0530

    signup req failing issue

--- apps/http-server/src/middleware/middleware.ts
+++ apps/http-server/src/middleware/middleware.ts
@@ -1,20 +1,33 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "@repo/backend-common/index";
 
-
-export function middleware(req: Request, res: Response, next: NextFunction) {
-    const token = req.headers["authorization"] ?? "";
-
-    const decoded = jwt.verify(token, JWT_SECRET);
-
-    if (decoded) {
-        // @ts-ignore: TODO: Fix this
-        req.userId = decoded.userId;
-        next();
-    } else {
-        res.status(403).json({
-            message: "Unauthorized"
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const token = req.headers.authorization?.split(" ")[1]
+        if (!token) {
+            res.status(401).json({
+                message: "Token is not provided!",
+                success: false
+            })
+            return
+        }
+        jwt.verify(token, process.env.SCERET!, function (err, data) {
+            if (err) {
+                return res.status(401).json({
+                    message: "Unauthorized",
+                    success: false,
+                })
+            }
+            if (typeof data !== "string") {
+                req.userId = data?.userId
+            }
+            next()
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: "Something went wrong server side",
+            success: false
         })
     }
 }
--- apps/http-server/src/routes/userRouter.ts
+++ apps/http-server/src/routes/userRouter.ts
@@ -1,36 +1,47 @@
 import express from 'express';
 import { JWT_SECRET } from "@repo/backend-common/index"
+import bcrypt, { hash } from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const userRouter: express.Router = express.Router();
 
-userRouter.get('/signup',async  (req, res) => {
-    const { CreateUserSchema } = await import("@repo/common/index")
+userRouter.post('/signup', async (req, res) => {
+    const { CreateUserSchema } = await import('@repo/common/index');
     const parsedData = CreateUserSchema.safeParse(req.body);
+
     if (!parsedData.success) {
-        console.log(parsedData.error);
-        res.json({
-            message: "Incorrect inputs"
-        })
-        return;
+        console.error(parsedData.error);
+        res.status(400).json({ message: 'Invalid input data' });
+        return
     }
+
     try {
-        const { prisma } = await import("@repo/db/index");
+        const { prisma } = await import('@repo/db/index');
+        const hashedPassword = await bcrypt.hash(parsedData.data.password, 10);
         const user = await prisma.user.create({
             data: {
-                email: parsedData.data?.username,
-                // TODO: Hash the pw
-                password: parsedData.data.password,
-                name: parsedData.data.name
-            }
-        })
-        res.json({
-            userId: user.id
-        })
-    } catch(e) {
-        res.status(411).json({
-            message: "User already exists with this username"
-        })
+                email: parsedData.data.username,
+                password: hashedPassword,
+                name: parsedData.data.name,
+            },
+        });
+        const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: '1h' });
+
+        res.status(201).json({
+            userId: user.id,
+            token,
+            message: 'User Signed Up Successfully',
+        });
+    } catch (error: any) {
+        console.error(error);
+        if (error.code === 'P2002') {
+            res.status(409).json({ message: 'User already exists' });
+            return
+        }
+
+        res.status(500).json({ message: 'Internal Server Error' });
     }
-})
+});
+
 
 export default userRouter;
