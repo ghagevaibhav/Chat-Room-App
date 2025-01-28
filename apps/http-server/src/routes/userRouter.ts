b584de70539c7e7be183ceac11f268b218addd3b
ghagevaibhav <ghagevaibhav@gmail.com>
2025-01-28 19:31:56 +0530

    update

--- apps/http-server/src/routes/userRouter.ts
+++ apps/http-server/src/routes/userRouter.ts
@@ -1,82 +1,83 @@
-import express from 'express';
-import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
-const { CreateUserSchema, SigninSchema } = await import('@repo/common/index');
-const { JWT_SECRET } = await import("@repo/backend-common/index");
-const { prisma } = await import('@repo/db/index');
+    import express from 'express';
+    import bcrypt from "bcrypt"
+    import jwt from "jsonwebtoken"
+    const { CreateUserSchema, SigninSchema } = await import('@repo/common/index');
+    const { JWT_SECRET } = await import("@repo/backend-common/index");
+    const { prisma } = await import('@repo/db/index');
 
-const userRouter: express.Router = express.Router();
+    const userRouter: express.Router = express.Router();
 
-userRouter.post('/signup', async (req, res) => {
-    
-    const parsedData = CreateUserSchema.safeParse(req.body);
+    userRouter.post('/signup', async (req, res) => {
+        
+        const parsedData = CreateUserSchema.safeParse(req.body);
 
-    if (!parsedData.success) {
-        console.error(parsedData.error);
-        res.status(400).json({ message: 'Invalid input data' });
-        return
-    }
+        if (!parsedData.success) {
+            console.error(parsedData.error);
+            res.status(400).json({ message: 'Invalid input data' });
+            return
+        }
 
-    try {
-        bcrypt.hash(parsedData.data.password, 10, async (err, hashedPassword) =>{
-            const user = await prisma.user.create({
-                data: {
-                    email: parsedData.data.username,
-                    password: hashedPassword,
-                    name: parsedData.data.name,
-                },
+        try {
+            bcrypt.hash(parsedData.data.password, 10, async (err, hashedPassword) =>{
+                if(err) throw err;
+                const user = await prisma.user.create({
+                    data: {
+                        email: parsedData.data.username,
+                        password: hashedPassword,
+                        name: parsedData.data.name,
+                    },
+                });
+                const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: '1h' });
+                
+                res.status(201).json({
+                    userId: user.id,
+                    token,
+                    hashedPassword,
+                    message: 'User Signed Up Successfully',
+                });
             });
+        } catch (error: any) {  
+            console.error(error);
+            res.status(500).json({ message: 'Internal Server Error' });
+            return;
+        }
+    });
+    userRouter.post('/signin', async (req, res) => {
+        const parsedData = SigninSchema.safeParse(req.body);
+        if (!parsedData.success) {
+            console.error(parsedData.error);
+            res.status(400).json({ message: 'Invalid input data' });
+            return;
+        }
+
+        try {
+            const { username, password } = parsedData.data;
+
+            const user = await prisma.user.findUnique({
+                where: { email: username }
+            });
+            if (!user || !user.password) {
+                res.status(404).json({ message: 'User not found' });
+                return;
+            }
+
+            const same = await bcrypt.compare(password, user.password);
+            if (!same) {
+                res.status(401).json({ message: 'Invalid password' });
+                return;
+            }
+
             const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: '1h' });
-            
-            res.status(201).json({
+            res.json({
                 userId: user.id,
-                token,
-                hashedPassword,
-                message: 'User Signed Up Successfully',
+                token: token,
+                message: 'User Logged In Successfully'
             });
-        });
-    } catch (error: any) {  
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
-        return;
-    }
-});
-userRouter.post('/signin', async (req, res) => {
-    const parsedData = SigninSchema.safeParse(req.body);
-    if (!parsedData.success) {
-        console.error(parsedData.error);
-        res.status(400).json({ message: 'Invalid input data' });
-        return;
-    }
-
-    try {
-        const { username, password: enteredPassword } = parsedData.data;
-
-        const user = await prisma.user.findUnique({
-            where: { email: username }
-        });
-        if (!user || !user.password) {
-            res.status(404).json({ message: 'User not found' });
             return;
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Internal Server Error' });
         }
+    });
 
-        const same = await bcrypt.compare(enteredPassword, user.password);
-        if (!same) {
-            res.status(401).json({ message: 'Invalid password' });
-            return;
-        }
-
-        const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: '1h' });
-        res.json({
-            userId: user.id,
-            token: token,
-            message: 'User Logged In Successfully'
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
-});
-
-
-export default userRouter;
+    export default userRouter;
