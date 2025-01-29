8dcf82044ed143e835f7463251cbddc80733c672
ghagevaibhav <ghagevaibhav@gmail.com>
2025-01-30 00:13:24 +0530

    ws room logic added

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,9 +1,82 @@
-import { WebSocket, WebSocketServer } from 'ws';
+import { WebSocket, WebSocketServer } from "ws";
+import jwt, { decode } from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/index";
+import { prisma } from "@repo/db/index";
 
-const wss = new WebSocketServer({port: 8080})
+const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection (ws: WebSocket) {
-    ws.on('message', function message(data: string) {
-        ws.send("Pong!");
-    })
-})
+interface User {
+  ws: WebSocket;
+  userId: string;
+  rooms: string[];
+}
+
+const users: User[] = [];
+
+function checkUser(token: string): string | null {
+  const decoded = jwt.verify(token, JWT_SECRET);
+
+  if (typeof decoded === "string") {
+    return null;
+  }
+  if (!decoded || !decoded.id) {
+    return null;
+  }
+  return decoded.id;
+}
+
+wss.on("connection", function connection(ws: WebSocket, req: Request) {
+  const url = req.url;
+  const queryParam = new URLSearchParams(url.split("?")[1]);
+  const token = queryParam.get("token") || "";
+  const userId = checkUser(token);
+
+  if (!userId) {
+    ws.send("Unauthorized");
+    ws.close();
+    return;
+  }
+
+  users.push({
+    userId,
+    ws,
+    rooms: [],
+  });
+
+  ws.on("message", async function message(data: string) {
+    const parsedData = JSON.parse(data);
+
+    if (parsedData.type === "join_room") {
+      const user = users.find((x) => x.ws === ws);
+      const room = await prisma.room.findUnique({
+        where: {
+          id: parsedData.roomId,
+        },
+      });
+      if (!room) {
+        ws.send("Room not found");
+        return;
+      }
+      user?.rooms.push(parsedData.roomId);
+    }
+
+    if (parsedData.type === "leave_room") {
+      const user = users.find((x) => x.ws === ws);
+      if (!user) return;
+      const roomIndex = user.rooms.findIndex((x) => x === parsedData.roomId);
+      if (roomIndex) {
+        user.rooms.splice(roomIndex, 1);
+      }
+    }
+
+    if (parsedData.type === "chat") {
+        const room = parsedData.roomId;
+        const message = parsedData.message;
+        users.forEach((user) => {
+            if(user.rooms.includes(room)){
+                user.ws.send(JSON.stringify({ type: "chat", message, room }));
+            }
+        })
+    }
+  });
+});
